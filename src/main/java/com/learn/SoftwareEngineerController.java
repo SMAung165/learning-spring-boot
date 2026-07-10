fdd8d2f797b0ff4a0b44629ea65ade93311133c9
sma <soemoe.aung8332@gmail.com>
2026-07-10 22:28:20 +0700

    Third commit

--- src/main/java/com/learn/SoftwareEngineerController.java
+++ src/main/java/com/learn/SoftwareEngineerController.java
@@ -1,9 +1,7 @@
 package com.learn;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +17,10 @@ public class SoftwareEngineerController {
     @GetMapping
     public List<SoftwareEngineer> getEngineers(){
        return softwareEngineerService.getAllSoftwareEngineers();
+    }
+
+    @PostMapping
+    public void addNewSoftwareEngineer(@RequestBody SoftwareEngineer softwareEngineer){
+        softwareEngineerService.insertSoftwareEngineer(softwareEngineer);
     }
 }
